347bb9c7496144e2268bf7c26c5cc3ad7860b308
Arman Afridi <armanafridi1203@gmail.com>
2026-06-26 11:03:02 +0530

    ...

---
+++ Class Work/Day 2/Eligibilty for voting.py
@@ -0,0 +1,9 @@
+# Problem Statement:
+# To find out eligibilty for a person to vote .
+# A person is eligible if the Age of the person is  18 or above 18.  
+
+Age = int(input("Enter your Age:"))
+if(Age>=18):
+    print("you are eligibile to vote")
+else:
+    print("you are not eligible to vote")
---
+++ Class Work/Fuel_consumption_tracker.py
@@ -0,0 +1,8 @@
+distance = float(input("Enter total distance traveled (km): "))
+fuel = float(input("Enter fuel consumed (liters): "))
+
+# Calculate average mileage
+mileage = distance / fuel
+
+# Output
+print("Average Mileage =", mileage, "km/l")
